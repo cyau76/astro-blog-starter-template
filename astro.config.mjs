f7c6e92941c192d57a697db02e1940516ba73f53
cyau76 <127944745+cyau76@users.noreply.github.com>
2026-02-24 22:58:11 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://YOUR-PROJECT.pages.dev",
+  integrations: [mdx(), sitemap()],
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/compile",
+    },
+  },
+  adapter: cloudflare({
+    platformProxy: { enabled: true },
+  }),
 });
